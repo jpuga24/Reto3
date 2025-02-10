1836488c94e538214951ebdc6cde0ba2329bce86
Miguel Campo <125380860+McMac31@users.noreply.github.com>
2025-02-10 10:58:49 +0100

    Delete PracticaEmpleados directory

--- PracticaEmpleados/EmpleadoTiempoCompleto.java
+++
@@ -1,9 +0,0 @@
-public class EmpleadoTiempoCompleto extends Empleado {
-    public int BonoAdicional= 1000;
-    public EmpleadoTiempoCompleto(String Nombre, int SalarioBase, int BonoAdicional){
-        super(Nombre, SalarioBase);
-        this.BonoAdicional=BonoAdicional;
-    }
-
-}
-
